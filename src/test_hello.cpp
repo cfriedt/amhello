d606c22da72c9b20ecd882e0987a8b73568835d6
Christopher Friedt <chrisfriedt@gmail.com>
2017-09-14 18:46:30 +0000

    added test that will fail

--- src/test_hello.cpp
+++ src/test_hello.cpp
@@ -18,3 +18,16 @@ TEST(HelloTest, hi) {
 		<< " do not match"
 	;
 }
+
+TEST(HelloTest, hi) {
+	std::string expected_str = "Sayonara!";
+	std::string actual_str = std::string( greeting() );
+
+	EXPECT_EQ( expected_str, actual_str )
+		<< "strings "
+		<< "'" << expected_str << "'"
+		<< " and "
+		<< "'" << actual_str << "'"
+		<< " do not match"
+	;
+}
